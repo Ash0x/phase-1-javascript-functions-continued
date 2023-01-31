6349020b91de296dbb5113a2c715b6cce2016f6e
Alexander <alex.galev@yahoo.com>
2023-01-31 10:01:15 -0700

    passing tests

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+const saturdayFun = (activity) => {
+	if (activity) return `This Saturday, I want to ${activity}!`
+	else return `This Saturday, I want to roller-skate!`
+}
+
+const mondayWork = (activity) => {
+	if (activity) return `This Monday, I will ${activity}.`
+	else return `This Monday, I will go to the office.`
+}
+
+const wrapAdjective = (flair = '*') => {
+	const encouragingPromptFunction = (adjective = 'special') => {
+		return `You are ${flair}${adjective}${flair}!`
+	}
+    return encouragingPromptFunction
+}
